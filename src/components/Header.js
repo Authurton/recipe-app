c7379b8f07be9682d6208df2f472753570d52f7a
authurton lunga <authurlunga@gmail.com>
2021-11-09 13:05:39 +0200

    Remove unused packages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState} from 'react';
-import axios from 'axios';
-
+import React from 'react';
 
 
 function Header({setSearchQuery, query}) {
--- src/components/RecipeComponent.js
+++ src/components/RecipeComponent.js
@@ -1,7 +1,5 @@
-import { render } from "@testing-library/react";
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import "./RecipeComponent.css"
 
 
@@ -11,7 +9,7 @@ const RecipeComponent = () => {
 
         console.log(recipes)
     const renderList =   recipes.map((recipe) => {
-        const {id, healthLabels,text, recipe:{ingredients}, ingredientLines, label, image} = recipe;
+        const {id,text, recipe:{ingredients},label, image} = recipe;
 
         if(ingredients && ingredients.length === 0){
             return <p>No ingredients yet....</p>
@@ -19,7 +17,7 @@ const RecipeComponent = () => {
 
         return(
             <div>
-                <img className="recipe__image" src={recipe.recipe.image} alt="Taco image"/>
+                <img className="recipe__image" src={recipe.recipe.image} alt="Tacos..."/>
                 <div className="recipe__label">{recipe.recipe.label}  </div>
                 <div >
                     {
